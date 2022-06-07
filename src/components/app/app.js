3fb1e29c69657edcbbee1f43e0c714d66e7f023e
Nikita <nosbergduo@mail.ru>
2022-06-08 00:51:47 +0300

    finish

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
                 {name: 'Osipov Sergei', salary: 5000, increase: false, like: true, id: 1},
                 {name: 'Fefelova Rimma', salary: 1200, increase: false, like: false, id: 2},
                 {name: 'Mantul Milena', salary: 3000, increase: false, like: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -58,12 +60,12 @@ class App extends Component {
         
     }
 
-    onToggleIncrease = (id) => {
+    onToggleProp = (id, prop) => {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
 
             const old = data[index];
-            const newItem = {...old, increase: !old.increase};
+            const newItem = {...old, [prop]: !old[prop]};
             const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
             return {
                 data: newArr
@@ -71,34 +73,50 @@ class App extends Component {
         })
     }
 
-    onToggleLike = (id) => {
-        this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
 
-            const old = data[index];
-            const newItem = {...old, like: !old.like};
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-            return {
-                data: newArr
-            }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
         })
     }
 
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+   filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.like);
+            case 'moreThan1000': 
+                return items.filter(item => item.salary >= 1000)
+            default:
+                return items;
+        }
+   }
+
+   onFilterSelect = (filter) => {
+        this.setState({filter});
+   }
 
     render() {
+        const {data, term, filter} = this.state;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
         return (
             <div className="app">
-                <AppInfo data={this.state.data}/>
+                <AppInfo data={data}/>
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
-                    onToggleIncrease={this.onToggleIncrease}
-                    onToggleLike={this.onToggleLike}/>
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,7 +2,7 @@ import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
     
-        const {name, salary, onDelete, onToggleIncrease, onToggleLike, increase, like} = props;
+        const {name, salary, onDelete, onToggleProp, increase, like} = props;
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
@@ -15,7 +15,7 @@ const EmployeesListItem = (props) => {
 
         return (
             <li className={classNames}>
-                <span className="list-group-item-label" onClick={onToggleLike}>{name}</span>
+                <span className="list-group-item-label" onClick={onToggleProp} data-toggle='like'>{name}</span>
                 <input 
                     type="text" 
                     className="list-group-item-input"
@@ -24,7 +24,8 @@ const EmployeesListItem = (props) => {
                         <button 
                             type="button"
                             className="btn-cookie btn-sm"
-                            onClick={onToggleIncrease}>
+                            onClick={onToggleProp}
+                            data-toggle='increase'>
                                 <i className="fas fa-cookie"></i>
                         </button>
                         <button 
